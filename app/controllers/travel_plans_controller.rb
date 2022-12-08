b196620ef68359c00904682d8813546d1c9f0451
akiasa oshima <asaolr4@gmail.com>
2022-12-09 00:27:50 +0900

    チームと紐ずくように設定

--- app/controllers/travel_plans_controller.rb
+++ app/controllers/travel_plans_controller.rb
@@ -1,8 +1,9 @@
 class TravelPlansController < ApplicationController
   before_action :set_travel_plan, only: [:show, :edit, :update, :destroy]
+  before_action :set_team, only: [:index, :new, :edit, :create]
 
   def index
-    @travel_plans = TravelPlan.all
+    @travel_plans = Team.find(params[:team_id]).travel_plans
   end
 
   def show
@@ -16,10 +17,10 @@ class TravelPlansController < ApplicationController
   end
 
   def create
+    # binding.pry
     @travel_plan = TravelPlan.new(travel_plan_params)
-
     if @travel_plan.save
-      redirect_to @travel_plan, notice: 'Travel plan was successfully created.'
+      redirect_to team_travel_plans_path, notice: 'Travel plan was successfully created.'
     else
       render :new
     end
@@ -27,7 +28,7 @@ class TravelPlansController < ApplicationController
 
   def update
     if @travel_plan.update(travel_plan_params)
-      redirect_to @travel_plan, notice: 'Travel plan was successfully updated.'
+      redirect_to team_travel_plans_path, notice: 'Travel plan was successfully updated.'
     else
       render :edit
     end
@@ -35,15 +36,20 @@ class TravelPlansController < ApplicationController
 
   def destroy
     @travel_plan.destroy
-    redirect_to travel_plans_url, notice: 'Travel plan was successfully destroyed.'
+    redirect_to team_travel_plans_path, notice: 'Travel plan was successfully destroyed.'
   end
 
   private
-    def set_travel_plan
-      @travel_plan = TravelPlan.find(params[:id])
-    end
 
-    def travel_plan_params
-      params.require(:travel_plan).permit(:name, :start_schedule_at, :end_schedule_at, :team_id)
-    end
+  def set_travel_plan
+    @travel_plan = TravelPlan.find(params[:id])
+  end
+  
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
+
+  def travel_plan_params
+    params.require(:travel_plan).permit(:name, :start_schedule_at, :end_schedule_at).merge(team_id: params[:team_id])
+  end
 end
