3bce4a7a0876192900dac9f59c42174bce76067e
akiasa oshima <asaolr4@gmail.com>
2022-12-09 22:59:51 +0900

    ゲストログイン機能実装

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :ensure_normal_user, only: %i[update destroy]
+
+  def ensure_normal_user
+    if resource.email == 'guest@example.com' || resource.email == 'guest_admin@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーの更新・削除できません。'
+    end
+  end
+
   protected
   # アカウント編集後、プロフィール画面に移動する
   def after_update_path_for(resource)
