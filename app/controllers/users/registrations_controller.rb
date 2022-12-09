9d92f07cc464c7bd2b59c60efe527fcb47e14ac8
akiasa oshima <asaolr4@gmail.com>
2022-12-09 23:36:32 +0900

    ユーザーを編集できるように設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,11 +10,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   protected
-  # アカウント編集後、プロフィール画面に移動する
   def after_update_path_for(resource)
     user_path(id: current_user.id)
   end
 
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
   
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
