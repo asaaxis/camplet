49848a774a6ad7f250ce980b731262446e66c641
akiasa oshima <asaolr4@gmail.com>
2022-12-09 14:36:31 +0900

    ユーザーのギアを表示できるように修正

--- app/models/travel_plan.rb
+++ app/models/travel_plan.rb
@@ -4,4 +4,6 @@ class TravelPlan < ApplicationRecord
   belongs_to :team
   has_many :labellings, dependent: :destroy
   has_many :labels, through: :labellings
+  has_many :belongings, dependent: :destroy
+  has_many :gears, through: :belongings
 end
