16fcf4ff88ecbb8aed3aab8b169c5110de5010f4
akiasa oshima <asaolr4@gmail.com>
2022-12-12 18:15:13 +0900

    チャットの編集・削除機能追加

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,23 +1,57 @@
 class ChatsController < ApplicationController
+  before_action :set_travel_plan, only: [:create]
+
   def create
-    # Blogをパラメータの値から探し出し,Blogに紐づくcommentsとしてbuildします。
-    @travel_plan = TravelPlan.find(params[:travel_plan_id])
     @chat = @travel_plan.chats.build(chat_params)
     @chat.user_id = current_user.id
-    # クライアント要求に応じてフォーマットを変更
     respond_to do |format|
       if @chat.save
-        # byebug
-        format.html { redirect_to travel_plan_path(@travel_plan), notice: '投稿できました！' }
+        
+        flash.now[:notice] = '投稿できました！'
+        format.js { render :index } 
       else
         format.html { redirect_to travel_plan_path(@travel_plan), notice: '投稿できませんでした...' }
       end
     end
   end
 
+  def edit
+    @chat = Chat.find(params[:id])
+    respond_to do |format|
+      flash.now[:notice] = 'コメントの編集中'
+      format.js { render :edit }
+    end
+  end
+
+  def update
+    @chat = Chat.find(params[:id])
+    respond_to do |format|
+      if @chat.update(chat_params)
+        flash.now[:notice] = 'コメントが編集されました'
+        format.js { render :index }
+      else
+        flash.now[:notice] = 'コメントの編集に失敗しました'
+        format.js { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @chat = Chat.find(params[:id])
+    @chat.destroy
+    respond_to do |format|
+      flash.now[:notice] = 'コメントが削除されました'
+      format.js { render :index }
+    end
+  end
+
   private
-  # ストロングパラメーター
+
   def chat_params
     params.require(:chat).permit(:content, :team_id, :travel_plan_id, :user_id)
+  end
+
+  def set_travel_plan
+    @travel_plan = TravelPlan.find(params[:travel_plan_id])
   end
 end
