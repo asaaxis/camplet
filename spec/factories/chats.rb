945aa3fc31ca148ffb93efcf81fda3f511d24b8d
akiasa oshima <asaolr4@gmail.com>
2022-12-17 20:37:38 +0900

    チャットモデルテスト

---
+++ spec/factories/chats.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :chat, class: Chat do
+    content { 'よろしく' }
+  end
+end
---
+++ spec/models/chat_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'チャットモデル機能', type: :model do
+  let!(:user){ FactoryBot.create(:second_user) }
+  let!(:travel_plan){ FactoryBot.create(:travel_plan) }
+  describe 'バリデーションのテスト' do
+    context 'コメントを入力した場合' do
+      it 'バリデーションが通る' do
+        chat = FactoryBot.build(:chat, user: user, travel_plan: travel_plan)
+        expect(chat).to be_valid
+      end
+    end
+    context 'コメントが空の場合' do
+      it 'バリデーションにひっかかる' do
+        chat = FactoryBot.build(:chat, content: "", user: user, travel_plan: travel_plan)
+        expect(chat).to be_invalid
+      end
+    end
+  end
+end
+# bundle exec rspec spec/models/travel_plan_spec.rb
