2eab1fd1f6580119e5a1b17d066b32299808a0f5
akiasa oshima <asaolr4@gmail.com>
2022-12-22 11:23:47 +0900

    不要コメント削除

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,4 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
-
   def guest_sign_in
     user = User.guest
     sign_in user
@@ -14,26 +10,4 @@ class Users::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to user_path(user.id), notice: 'ゲスト管理者ユーザーとしてログインしました。'
   end
-
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
